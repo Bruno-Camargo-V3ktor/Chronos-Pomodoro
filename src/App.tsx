d3de0412cbc218003349e66f0996747f10bd613c
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-09-24 14:01:04 -0300

    feat: add react-toastify and notification messagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { HomePage } from './pages/Home';
+import { HomePage } from "./pages/Home";
 
-import './styles/theme.css';
-import './styles/global.css';
-import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
-
+import "./styles/theme.css";
+import "./styles/global.css";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
+import { MessagesContainer } from "./components/MessagesContainer";
 
 export const App = () => {
-
   return (
-      <TaskContextProvider>
+    <TaskContextProvider>
+      <MessagesContainer>
         <HomePage />
-      </TaskContextProvider>
-  )
-}
+      </MessagesContainer>
+    </TaskContextProvider>
+  );
+};
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,13 +1,14 @@
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react"
-import { Cycles } from "../Cycles"
-import { DefaultButton } from "../DefaultButton"
-import { DefaultInput } from "../DefaultInput"
-import type React from "react"
-import { useRef } from "react"
-import type { TaskModel } from "../../models/TaskModel"
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
-import { TaskActionTypes } from "../../contexts/TaskContext/taskActions"
-import { Tips } from "../Tips"
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
+import { DefaultButton } from "../DefaultButton";
+import { DefaultInput } from "../DefaultInput";
+import type React from "react";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
+import { Tips } from "../Tips";
+import { showMessage } from "../../adapters/showMessage";
 
 export const MainForm = () => {
   const { state, dispatch } = useTaskContext();
@@ -15,11 +16,12 @@ export const MainForm = () => {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
     if (taskNameInput.current === null) return;
-    
+
     const taskName = taskNameInput.current.value.trim();
     if (!taskName) {
-      alert("Digite o nome da tarefa");
+      showMessage.warn("Digite o nome da tarefa");
       return;
     }
 
@@ -30,48 +32,63 @@ export const MainForm = () => {
       completeDate: null,
       interruptDate: null,
       duration: 0,
-      type: 'workTime'
+      type: "workTime",
     };
 
-    dispatch({type: TaskActionTypes.START_TASK, playload: newTask })
+    dispatch({ type: TaskActionTypes.START_TASK, playload: newTask });
   }
-  
+
   function handleInterruptTask() {
-    dispatch({type: TaskActionTypes.INTERRUPT_TASK});
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
-        <>
-        <form onSubmit={handleCreateNewTask} className='form' action="">
-          <div className='formRow'>
-            <DefaultInput 
-              labelText='Tarefa' 
-              id='meuInput' 
-              type='text'
-              placeholder='Digite algo'
-              defaultValue='Minha Tarefa'
-              ref={taskNameInput}
-              disabled={!!state.activeTask}
+    <>
+      <form onSubmit={handleCreateNewTask} className="form" action="">
+        <div className="formRow">
+          <DefaultInput
+            labelText="Tarefa"
+            id="meuInput"
+            type="text"
+            placeholder="Digite algo"
+            defaultValue="Minha Tarefa"
+            ref={taskNameInput}
+            disabled={!!state.activeTask}
+          />
+        </div>
+
+        <div className="formRow">
+          <Tips />
+        </div>
+
+        {state.currentCycle > 0 && (
+          <div className="formRow">
+            <Cycles />
+          </div>
+        )}
+
+        <div className="formRow">
+          {!state.activeTask ? (
+            <DefaultButton
+              key="send-form"
+              aria-label="Iniciar nova tarefa"
+              title="Iniciar nova tarefa"
+              type="submit"
+              icon={<PlayCircleIcon />}
             />
-          </div>
-
-          <div className='formRow'>
-            <Tips />
-          </div>
-
-          { state.currentCycle > 0 &&
-            <div className='formRow'>
-              <Cycles />
-            </div>
-          }
-
-          <div className='formRow'>
-            {!state.activeTask 
-              ? <DefaultButton key='send-form' aria-label='Iniciar nova tarefa' title='Iniciar nova tarefa' type='submit' icon={<PlayCircleIcon/>} />
-              : <DefaultButton key='not-send-form' onClick={handleInterruptTask}  aria-label='Interromper tarefa atual' title='Interromper tarefa atual' type='button' color="red" icon={<StopCircleIcon/>} />
-            }
-          </div>
-        </form>
-        </>
-    )
-}
+          ) : (
+            <DefaultButton
+              key="not-send-form"
+              onClick={handleInterruptTask}
+              aria-label="Interromper tarefa atual"
+              title="Interromper tarefa atual"
+              type="button"
+              color="red"
+              icon={<StopCircleIcon />}
+            />
+          )}
+        </div>
+      </form>
+    </>
+  );
+};
