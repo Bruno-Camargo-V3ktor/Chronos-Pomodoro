be2b146cfcb59f46e052c02d134f243a2ad7f87c
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-09-24 14:27:06 -0300

    feat: create component for Main Routers and move to top page for change page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { HomePage } from "./pages/Home";
-
 import "./styles/theme.css";
 import "./styles/global.css";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { MessagesContainer } from "./components/MessagesContainer";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { NotFoundPage } from "./pages/NotFound";
-import { AboutPomodoroPage } from "./pages/AboutPomodoro";
+import { MainRouter } from "./routers/mainRouter";
 
 export const App = () => {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about-pomodoro" element={<AboutPomodoroPage />} />
-
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
---
+++ src/routers/mainRouter.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
+import { NotFoundPage } from "../pages/NotFound";
+import { AboutPomodoroPage } from "../pages/AboutPomodoro";
+import { HomePage } from "../pages/Home";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
+}
+
+export function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-pomodoro" element={<AboutPomodoroPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+}
