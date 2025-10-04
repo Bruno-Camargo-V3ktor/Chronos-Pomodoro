48d74a11e627d08861a05df18985815c55b3fa72
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-10-03 21:47:27 -0300

    feat: map task for field Status task

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -7,6 +7,7 @@ import { MainTemplate } from "../../templates/MainTemplate"
 import styles from './styles.module.css';
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 import { formatDate } from "../../utils/formatDate"
+import { getTaskStatus } from "../../utils/getTaskStatus"
 
 export const HistoryPage = () => {
     const { state } = useTaskContext();
@@ -47,7 +48,7 @@ export const HistoryPage = () => {
                                         <td>{task.name}</td>
                                         <td>{task.duration}</td>
                                         <td>{formatDate(task.startDate)}</td>
-                                        <td>{task.interruptDate}</td>
+                                        <td>{getTaskStatus(task, state.activeTask)}</td>
                                         <td>{task.type}</td>
                                     </tr>
                                 )
