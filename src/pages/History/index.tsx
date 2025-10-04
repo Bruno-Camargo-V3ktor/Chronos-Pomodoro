48b8b7dd55bd47e5817d344ccbdb83597a4d8dfc
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-10-03 21:43:30 -0300

    feat: add real state for table history and formatDate

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -5,8 +5,12 @@ import { Heading } from "../../components/Heading"
 import { MainTemplate } from "../../templates/MainTemplate"
 
 import styles from './styles.module.css';
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
+import { formatDate } from "../../utils/formatDate"
 
 export const HistoryPage = () => {
+    const { state } = useTaskContext();
+
     return (
         <MainTemplate>
             <Container>
@@ -37,13 +41,17 @@ export const HistoryPage = () => {
                         </thead>
 
                         <tbody>
-                            <tr>
-                                <td>Estudar</td>
-                                <td>25min</td>
-                                <td>20/04/2025 08:00</td>
-                                <td>Completa</td>
-                                <td>Foco</td>
-                            </tr>
+                            {state.tasks.map(task => {
+                                return (
+                                    <tr key={task.id}>
+                                        <td>{task.name}</td>
+                                        <td>{task.duration}</td>
+                                        <td>{formatDate(task.startDate)}</td>
+                                        <td>{task.interruptDate}</td>
+                                        <td>{task.type}</td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
                 </div>
