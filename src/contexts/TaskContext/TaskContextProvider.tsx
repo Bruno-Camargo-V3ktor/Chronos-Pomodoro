2f3c4f7afab9620b3730f779ed34741abc31bc29
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-10-03 21:22:54 -0300

    feat: save state in localStorage

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,36 +5,51 @@ import { taskReduce } from "./taskReduce";
 import { TimerWorkerManager } from "../../works/TimerWorkerManager";
 import { TaskActionTypes } from "./taskActions";
 import { loadBeep } from "../../utils/loadBeep";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
 type TaskContextProviderProps = {
     children: React.ReactNode
 }
 
-export const TaskContextProvider = ({children}: TaskContextProviderProps) => {
-    const [state, dispatch] = useReducer(taskReduce, initialState);
+export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
+    const [state, dispatch] = useReducer(taskReduce, initialState, () => {
+        const storageState = localStorage.getItem('state');
+
+        if (!storageState) return initialState;
+        const parsedStorageState = JSON.parse(storageState) as TaskStateModel;
+
+        return {
+            ...parsedStorageState,
+            activeTask: null,
+            secondsRemaining: 0,
+            formattedSecondsRemaining: '00:00',
+        };
+    });
     const playBeepRef = useRef<() => void | null>(null);
 
     const worker = TimerWorkerManager.getInstance();
-    worker.onMessage( e => {
+    worker.onMessage(e => {
         const countDownSeconds = e.data;
 
-        if( countDownSeconds <= 0 ) {
+        if (countDownSeconds <= 0) {
             if (playBeepRef.current) {
                 playBeepRef.current();
             }
-            
-            dispatch({type: TaskActionTypes.COMPLETE_TASK});
+
+            dispatch({ type: TaskActionTypes.COMPLETE_TASK });
             worker.terminate();
         } else {
             dispatch({
-            type: TaskActionTypes.COUNT_DOWN,
-            playload: {secondsRemaining: countDownSeconds}
-        });
+                type: TaskActionTypes.COUNT_DOWN,
+                playload: { secondsRemaining: countDownSeconds }
+            });
         }
     });
 
     useEffect(() => {
-        if(!state.activeTask) {
+        localStorage.setItem('state', JSON.stringify(state));
+
+        if (!state.activeTask) {
             worker.terminate();
         }
 
@@ -52,8 +67,8 @@ export const TaskContextProvider = ({children}: TaskContextProviderProps) => {
     }, [state.activeTask])
 
     return (
-        <TaskContext.Provider value={{state, dispatch}}>
+        <TaskContext.Provider value={{ state, dispatch }}>
             {children}
-        </TaskContext.Provider>    
+        </TaskContext.Provider>
     )
 }
