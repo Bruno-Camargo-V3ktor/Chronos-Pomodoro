127237fac27d1d74937edef1bdba715bc4d51068
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-10-17 13:21:42 -0300

    feat: create onSubmit function for save settings and validation for inputs

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,44 +1,106 @@
-import { SaveIcon } from "lucide-react"
-import { Container } from "../../components/Container"
-import { DefaultButton } from "../../components/DefaultButton"
-import { DefaultInput } from "../../components/DefaultInput"
-import { Heading } from "../../components/Heading"
-import { MainTemplate } from "../../templates/MainTemplate"
-
+import { SaveIcon } from "lucide-react";
+import { Container } from "../../components/Container";
+import { DefaultButton } from "../../components/DefaultButton";
+import { DefaultInput } from "../../components/DefaultInput";
+import { Heading } from "../../components/Heading";
+import { MainTemplate } from "../../templates/MainTemplate";
+import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { showMessage } from "../../adapters/showMessage";
 
 export const SettingsPage = () => {
-    return (
-        <MainTemplate>
-            <Container>
-                <Heading>Configurações</Heading>
-            </Container>
+  const { state } = useTaskContext();
+  const workTimeInputRef = useRef<HTMLInputElement | null>(null);
+  const shortBreakTimeInputRef = useRef<HTMLInputElement | null>(null);
+  const longBreakTimeInputRef = useRef<HTMLInputElement | null>(null);
 
-            <Container>
-                <p style={{ textAlign: 'center' }}>
-                    Modifique as configurações para tempo de foco, descanso curso e
-                    descanso longo.
-                </p>
-            </Container>
+  function handleSaveSettings(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-            <Container>
-                <form action='' className='form'>
-                    <div className='formRow'>
-                        <DefaultInput id='workTime' labelText="Foco" />
-                    </div>
+    const formErros = [];
 
-                    <div className='formRow'>
-                        <DefaultInput id='shortBreakTime' labelText="Descanso Curto" />
-                    </div>
+    const workTime = Number(workTimeInputRef.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInputRef.current?.value);
+    const longBreakTime = Number(longBreakTimeInputRef.current?.value);
 
-                    <div className='formRow'>
-                        <DefaultInput id='longBreakTime' labelText="Descanso Longo" />
-                    </div>
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErros.push("Por favor use apenas numeros para o Foco.");
+    }
+    if (isNaN(shortBreakTime)) {
+      formErros.push("Por favor use apenas numeros para o Descanso Curto.");
+    }
+    if (isNaN(longBreakTime)) {
+      formErros.push("Por favor use apenas numeros para o Descanso Longo.");
+    }
 
-                    <div className='formRow'>
-                        <DefaultButton icon={<SaveIcon />} aria-label="Salva Configurações" />
-                    </div>
-                </form>
-            </Container>
-        </MainTemplate>
-    )
-}
+    if (workTime < 1 || workTime > 99) {
+      formErros.push("Digite valores entre '1' e '99' para o Foco.");
+    }
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErros.push("Digite valores entre '1' e '30' para o Descanso Curto.");
+    }
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErros.push("Digite valores entre '1' e '60' para o Descanso Longo.");
+    }
+
+    if (formErros.length > 0) {
+      formErros.forEach((e) => {
+        showMessage.error(e);
+      });
+      return;
+    }
+  }
+
+  return (
+    <MainTemplate>
+      <Container>
+        <Heading>Configurações</Heading>
+      </Container>
+
+      <Container>
+        <p style={{ textAlign: "center" }}>
+          Modifique as configurações para tempo de foco, descanso curso e
+          descanso longo.
+        </p>
+      </Container>
+
+      <Container>
+        <form onSubmit={handleSaveSettings} action="" className="form">
+          <div className="formRow">
+            <DefaultInput
+              id="workTime"
+              labelText="Foco"
+              ref={workTimeInputRef}
+              defaultValue={state.config.workTime}
+            />
+          </div>
+
+          <div className="formRow">
+            <DefaultInput
+              id="shortBreakTime"
+              labelText="Descanso Curto"
+              ref={shortBreakTimeInputRef}
+              defaultValue={state.config.shortBreakTime}
+            />
+          </div>
+
+          <div className="formRow">
+            <DefaultInput
+              id="longBreakTime"
+              labelText="Descanso Longo"
+              ref={longBreakTimeInputRef}
+              defaultValue={state.config.longBreakTime}
+            />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton
+              icon={<SaveIcon />}
+              aria-label="Salva Configurações"
+            />
+          </div>
+        </form>
+      </Container>
+    </MainTemplate>
+  );
+};
